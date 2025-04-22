a2c5b61b53c71cdabdd0bebc7aaf9ba0c558b130
SarahMaksour <sarahmaksour1@gmail.com>
2025-04-22 20:38:17 +0300

    model

---
+++ app/Models/Course_Unlock.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course_Unlock extends Model
+{
+    use HasFactory;
+}
