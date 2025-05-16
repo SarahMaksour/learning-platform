41a6a20434586f2f20c04bf4e01fcd329acd7912
SarahMaksour <sarahmaksour1@gmail.com>
2025-05-16 14:21:13 +0300

    add userresource

--- app/Http/Controllers/home/HomeController.php
+++ app/Http/Controllers/home/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\home;
 use Illuminate\Http\Request;
 use App\Services\Api\HomeService;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\Http\Resources\CourseResource;
 
 class HomeController extends Controller
@@ -17,12 +18,12 @@ class HomeController extends Controller
 public function homePage(){
     $featuredPopular=$this->homeService->getPopularCourses();
     $featuredTopRated=$this->homeService->getTopRatedCourses();
-  //  $user=auth()->user();
+   $user=auth()->user();
     return response()->json([
+         'user' => new UserResource($user),
         'popular_courses' => CourseResource::collection($featuredPopular),
         'top_rated_courses' => CourseResource::collection($featuredTopRated),
-    ],201);
+        ]
+    ,201);
 }
-
-
 }
