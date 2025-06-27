b28980e1c8bd89133d23d5f015fdf0d9a22dd79e
SarahMaksour <sarahmaksour1@gmail.com>
2025-06-27 22:59:36 +0300

    add images to user

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Video extends Model
 {
     use HasFactory;
-     protected $fillable = ['title', 'video_path', 'duration'];
+     protected $fillable = ['title', 'video_path', 'duration','description'];
 
 protected $hidden = ['courseContent'];
 
