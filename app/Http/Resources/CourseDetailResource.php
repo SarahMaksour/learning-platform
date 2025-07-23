4c1a2486ee5e81d6438d01ae64b220922ec38792
SarahMaksour <sarahmaksour1@gmail.com>
2025-07-23 23:42:09 +0300

    add course section for instructor

--- app/Http/Resources/CourseDetailResource.php
+++ app/Http/Resources/CourseDetailResource.php
@@ -21,7 +21,7 @@ class CourseDetailResource extends JsonResource
             'description' => $this->description,
             'image_course' =>$this->image? asset($this->image) : null,
             'price' => $this->price,
-            'isPaid'=>$this->isPaid
+            'isPaid'=>$this->isPaid,
             'reviews_count' => $this->reviews_count,
             'reviews_avg_rating' => round($this->reviews_avg_rating, 2),
             'students_count' => $this->enrollments_count,
