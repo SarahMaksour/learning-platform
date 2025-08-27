1dbfd642b09a9a9f6f119c33a4fb043e9fa3b22c
Haneen012 <haninalsaidali@gmail.com>
2025-08-27 15:01:16 -0700

    editfilter2

--- app/Services/FilterService.php
+++ app/Services/FilterService.php
@@ -27,30 +27,6 @@ class FilterService
         }
 
         $courses = $query->get(['id', 'title', 'price', 'image', 'user_id']);
-
-        // حساب مدة الفيديوهات يدويًا
-        foreach ($courses as $course) {
-            $totalDuration = 0;
-            foreach ($course->contents as $content) {
-                if ($content->contentable_type === \App\Models\Video::class && $content->contentable) {
-                    $totalDuration += $content->contentable->duration;
-                }
-            }
-            $course->setAttribute('total_video_duration', $totalDuration);
-        }
-
-        // فلترة على مدة الفيديوهات باستخدام Collection
-        if ($request->filled('minDuration') && $request->filled('maxDuration')) {
-            $min = $request->minDuration * 60;
-            $max = $request->maxDuration * 60;
-            if ($min > $max) {
-                [$min, $max] = [$max, $min];
-            }
-            $courses = $courses->filter(function ($course) use ($min, $max) {
-                return $course->total_video_duration >= $min && $course->total_video_duration <= $max;
-            })->values();
-        }
-
         return $courses;
     }
 }
