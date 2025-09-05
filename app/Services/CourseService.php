dbcf76c575fc892d3ee71baffa38c06feea5bfbd
SarahMaksour <sarahmaksour1@gmail.com>
2025-09-06 01:51:31 +0300

    review course

--- app/Services/CourseService.php
+++ app/Services/CourseService.php
@@ -53,6 +53,10 @@ class CourseService
 public function getCourseLessonsWithStatus($course_id, $user)
 {
     $course = Course::with('contents')->findOrFail($course_id);
+ $lessons = $course->contents->sortBy('id')->values();
+
+    // إذا المستخدم مسجّل دخول نتحقق إذا اشترى الكورس
+    $isPaid = $user ? $this->isUserPaid($user, $course) : false;
 
     // إذا الزائر مو مسجّل دخول
   /*  if (!$user) {
